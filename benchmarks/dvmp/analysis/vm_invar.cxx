ed8d64fd1fa0459d496a6f04b7308eb672481a11
Ziyue Zhang <Ziyue_Zhang@localhost.localdomain>
2021-03-25 16:56:28 -0500

    WIP: Factorize: test 16

--- benchmarks/dvmp/analysis/vm_invar.cxx
+++ benchmarks/dvmp/analysis/vm_invar.cxx
@@ -135,7 +135,7 @@ int vm_invar(const std::string& config_name)
   double hist_range_h[4] = {1., 15., 0.1, 0.};
   
   TString VarName[4] = {"y", "Q2", "x", "t"};
-  TH1D h_sim[4];
+  TH1D* h_sim[4];
   {
   auto h_tmp = d_im.Histo1D({"h_Q2_sim", ";Q^{2};#", 50, hist_range_l[0], hist_range_h[0]}, "Q2_sim");
   auto& htmp = *h_tmp;
