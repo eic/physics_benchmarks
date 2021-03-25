8270d5c43751d82406927f048d706681ffcbf7e3
Ziyue Zhang <Ziyue_Zhang@localhost.localdomain>
2021-03-25 17:08:30 -0500

    WIP: Factorize: test 18

--- benchmarks/dvmp/analysis/vm_invar.cxx
+++ benchmarks/dvmp/analysis/vm_invar.cxx
@@ -140,7 +140,7 @@ int vm_invar(const std::string& config_name)
   auto h_tmp = d_im.Histo1D({"h_Q2_sim", ";Q^{2};#", 50, hist_range_l[0], hist_range_h[0]}, "Q2_sim");
   auto& htmp = *h_tmp;
   
-  h_sim[0] = (TH1D*)htmp.Clone();
+  h_sim[0] = (TH1D*)h_tmp.Clone();
   }
   
   //TH1D h_sim[4];
