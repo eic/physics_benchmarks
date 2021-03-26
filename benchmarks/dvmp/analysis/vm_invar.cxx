5d3a5a05b4926c05b30bc6e0ddc0e0971e9d7785
Ziyue Zhang <Ziyue_Zhang@localhost.localdomain>
2021-03-25 19:22:54 -0500

    WIP: Factorize: test 28

--- benchmarks/dvmp/analysis/vm_invar.cxx
+++ benchmarks/dvmp/analysis/vm_invar.cxx
@@ -154,7 +154,9 @@ int vm_invar(const std::string& config_name)
   std::string_view histName_view = histName[0].View();
   std::string_view histTitle_view = histTitle[0].View();
   std::string_view RawHist_view = RawHist[0].View();
+  cout<<"==="<<histName_view<<"==="<<histTitle_view<<"==="<<RawHist_view<<"==="<<endl;
   auto h_tmp = d_im.Histo1D({histName_view, histTitle_view, 50, hist_range_l[0], hist_range_h[0]}, RawHist_view);
+  //auto h_tmp = d_im.Histo1D({"", "", 50, hist_range_l[0], hist_range_h[0]}, "");
   h_sim[0] = (TH1D*)h_tmp->Clone();
   }
   
