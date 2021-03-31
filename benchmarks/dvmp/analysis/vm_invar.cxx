3eb5843613d9aecadb9a220716d19ea1a6ccad96
Ziyue Zhang <Ziyue_Zhang@localhost.localdomain>
2021-03-31 01:59:19 -0500

    WIP: debug - hist def 01.4

--- benchmarks/dvmp/analysis/vm_invar.cxx
+++ benchmarks/dvmp/analysis/vm_invar.cxx
@@ -144,7 +144,7 @@ int vm_invar(const std::string& config_name)
       //construct RawhistName
       RawhistName[i][j] = VarName[i] + "_" + VarCate[j];
       //get histograms
-      //auto h_tmp = d_im.Histo1D({fmt::format("{}_tmp", histName[i][j]).c_str(), histTitles[i][j].c_str(), 50, range_l[i][j], range_h[i][j]}, RawhistName[i][j].c_str());
+      auto h_tmp = d_im.Histo1D({fmt::format("{}_tmp", histName[i][j]).c_str(), histTitles[i][j].c_str(), 50, range_l[i][j], range_h[i][j]}, RawhistName[i][j].c_str());
       //TH1D* hptr_tmp = &(*h_tmp);
       //h_Var1D[i][j] = (TH1D*)hptr_tmp->Clone(histName[i][j].c_str());
       //delete hptr_tmp;
