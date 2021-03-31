d6f71936800b0978c82593d7745be011523060cd
Ziyue Zhang <Ziyue_Zhang@localhost.localdomain>
2021-03-31 02:25:53 -0500

    WIP: debug - hist def 01.10.1

--- benchmarks/dvmp/analysis/vm_invar.cxx
+++ benchmarks/dvmp/analysis/vm_invar.cxx
@@ -158,7 +158,6 @@ int vm_invar(const std::string& config_name)
     for(int j = 0 ; j < 4 ; j++){
       TCanvas* ctest = new TCanvas("ctest", "ctest", 800,600);
       h_Var1D[i][j]->Draw("hist");
-      htmp.Draw("hist");
       ctest->Print(fmt::format("{}{}{}test.png", output_prefix, i, j).c_str());
       delete ctest;
     }
