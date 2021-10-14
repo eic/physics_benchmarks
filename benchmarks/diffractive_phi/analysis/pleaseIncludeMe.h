b79489f45a9114bae0627f4586176dde815052d8
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2021-10-14 17:46:44 -0400

    add

--- benchmarks/diffractive_phi/analysis/pleaseIncludeMe.h
+++ benchmarks/diffractive_phi/analysis/pleaseIncludeMe.h
@@ -30,13 +30,36 @@
 TH1D* h_mass = new TH1D("h_mass",";mass",200,0.,3.5);
 
 //particles properties
-auto momenta_from_reconstruction(const std::vector<eic::ReconstructedParticleData>& parts) {
+auto momenta_from_reconstruction_plus(const std::vector<eic::ReconstructedParticleData>& parts) {
   std::vector<ROOT::Math::PxPyPzEVector> momenta{parts.size()};
   std::transform(parts.begin(), parts.end(), momenta.begin(), [](const auto& part) {
-    return ROOT::Math::PxPyPzEVector{part.p.x, part.p.y, part.p.z, part.energy};
+    if(part.charge>0) return ROOT::Math::PxPyPzEVector{part.p.x, part.p.y, part.p.z, part.energy};
   });
   return momenta;
 }
+auto momenta_from_reconstruction_minus(const std::vector<eic::ReconstructedParticleData>& parts) {
+  std::vector<ROOT::Math::PxPyPzEVector> momenta{parts.size()};
+  std::transform(parts.begin(), parts.end(), momenta.begin(), [](const auto& part) {
+    if(part.charge<0)return ROOT::Math::PxPyPzEVector{part.p.x, part.p.y, part.p.z, part.energy};
+  });
+  return momenta;
+}
+
+auto vector_sum = [](std::vector<ROOT::Math::PxPyPzEVector> p1, 
+  std::vector<ROOT::Math::PxPyPzEVector> p2 ){
+  std::vector<ROOT::Math::PxPyPzEVector> vm;
+  for(auto& i1: p1){
+    for(auto& i2: p2){
+      //pt cut
+      if(i1.Pt()<0.15||i2.Pt()<0.15) continue;
+      //eta cut
+      //...
+      vm.push_back(i1+i2);
+    }
+  }
+  return vm;
+};
+
 auto getPt(const std::vector<ROOT::Math::PxPyPzEVector>& mom) {
   std::vector<double> PtVec(mom.size() );
   ROOT::Math::PxPyPzEVector beamMom = {0, 0, -18, 18};
@@ -45,3 +68,6 @@ auto getPt(const std::vector<ROOT::Math::PxPyPzEVector>& mom) {
   });
   return PtVec;
 }
+
+
+const Double_t dxbin = (0.17 - 0.13) / 40; // Bin-width
