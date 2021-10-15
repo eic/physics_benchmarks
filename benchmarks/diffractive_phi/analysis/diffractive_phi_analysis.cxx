3029d64d7e10527c9b68f311b3c1c8f4345a360c
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2021-10-15 16:01:58 -0400

    add

--- benchmarks/diffractive_phi/analysis/diffractive_phi_analysis.cxx
+++ benchmarks/diffractive_phi/analysis/diffractive_phi_analysis.cxx
@@ -66,7 +66,7 @@ int diffractive_phi_analysis(const std::string& config_name)
   auto h_Mass_rec = d1.Histo1D({"h_Mass_rec", "; GeV; counts", 1000, 0, 4}, "Mass");
   auto h_t_rec = d1.Histo1D({"h_t_rec", "; GeV^{2}; counts", 200, 0, 2}, "trec");
 
-  auto metCut = [](const ROOT::VecOps::RVec<int> x) { return x > 1.; };
+  auto metCut = [](const ROOT::VecOps::RVec<int> x) { return x[x>1]; };
   auto d2 = d.Define("scatID_value","InclusiveKinematicsElectron.scatID.value")
              .Define("scatID_source","InclusiveKinematicsElectron.scatID.source")
              .Define("scatElec",momenta_from_reconstruction_elect,{"ReconstructedChargedParticles"}).Filter(metCut, {"scatID_value"})
