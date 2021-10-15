0dbbdd60a2fdb6ada84613bb7265c08f9857458b
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2021-10-15 16:29:05 -0400

    add

--- benchmarks/diffractive_phi/analysis/diffractive_phi_analysis.cxx
+++ benchmarks/diffractive_phi/analysis/diffractive_phi_analysis.cxx
@@ -79,7 +79,7 @@ int diffractive_phi_analysis(const std::string& config_name)
   };
   auto d2 = d.Define("scatID_value","InclusiveKinematicsElectron.scatID.value")
              .Define("scatID_source","InclusiveKinematicsElectron.scatID.source")
-             .Define("scatID_cand_value",scatID_cand_value, "scatID_value")
+             .Define("scatID_cand_value",scatID_cand_value, {"scatID_value"})
              .Define("etaElec",getEta,{"scatElec"});
              
   // auto h_scatElec_eta = d2.Histo1D({"h_scatElec_eta",";eta; counts",100,0,PI}, "etaElec");
