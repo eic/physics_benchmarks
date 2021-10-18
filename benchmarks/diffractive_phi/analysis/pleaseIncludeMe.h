dcbd6500667b48becc380cc302f2087f7f9a66d8
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2021-10-18 18:54:39 -0400

    now onto mcparticles

--- benchmarks/diffractive_phi/analysis/pleaseIncludeMe.h
+++ benchmarks/diffractive_phi/analysis/pleaseIncludeMe.h
@@ -165,6 +165,7 @@ auto getEtaMC(const std::vector<ROOT::Math::PxPyPzMVector>& mom) {
   std::vector<double> etaVec(mom.size() );
   std::transform(mom.begin(), mom.end(), etaVec.begin(), [](const auto& part) {
     if(part.Px()<-1e9) return -99.;
+    else return part.Eta();
   });
   return etaVec;
 }
