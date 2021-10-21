f427d0f1a192baf5006c3e85966c51ce206fb6ee
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2021-10-21 18:21:41 -0400

    update analysis

--- benchmarks/dvcs-d/analysis/pleaseIncludeMe.h
+++ benchmarks/dvcs-d/analysis/pleaseIncludeMe.h
@@ -88,42 +88,6 @@ auto scatID_cand_value = [](const ROOT::VecOps::RVec<int>& x){
   return value;
 };
 
-auto momenta_from_reconstruction_plus(const std::vector<eic::ReconstructedParticleData>& parts) {
-  std::vector<ROOT::Math::PxPyPzMVector> momenta{parts.size()};
-  std::transform(parts.begin(), parts.end(), momenta.begin(), [](const auto& part) {
-    if(part.charge>0){
-      return ROOT::Math::PxPyPzMVector{part.p.x, part.p.y, part.p.z, vm_daug_mass[which_vm]};
-    }
-    else{
-      return ROOT::Math::PxPyPzMVector{-1e10, -1e10, -1e10, -1e10};
-    }
-  });
-  return momenta;
-}
-
-auto momenta_from_reconstruction_minus(const std::vector<eic::ReconstructedParticleData>& parts) {
-  std::vector<ROOT::Math::PxPyPzMVector> momenta{parts.size()};
-  std::transform(parts.begin(), parts.end(), momenta.begin(), [](const auto& part) {
-     if(part.charge<0){
-      return ROOT::Math::PxPyPzMVector{part.p.x, part.p.y, part.p.z, vm_daug_mass[which_vm]};
-    }
-    else{
-      return ROOT::Math::PxPyPzMVector{-1e10, -1e10, -1e10, -1e10};
-    }
-  });
-  return momenta;
-}
-
-bool sort_mom_bool(ROOT::Math::PxPyPzMVector &mom1, ROOT::Math::PxPyPzMVector &mom2) {
-  return  mom1.energy() > mom2.energy(); 
-}
-
-auto sort_momenta(const std::vector<ROOT::Math::PxPyPzMVector>& mom) {
-  std::vector <ROOT::Math::PxPyPzMVector> sort_mom = mom;
-  sort(sort_mom.begin(), sort_mom.end(), sort_mom_bool);
-  return sort_mom;
-}
-
 auto findScatElec(const std::vector<eic::ReconstructedParticleData>& parts, 
       std::vector<int> scat_id,
     std::vector<int> scat_source) 
@@ -150,7 +114,7 @@ auto findScatElecMC(const std::vector<dd4pod::Geant4ParticleData>& parts)
 {
   std::vector<ROOT::Math::PxPyPzMVector> momenta;
   for(auto& i1 : parts){
-    if(i1.genStatus==11&&i1.pdgID==11) {
+    if(i1.genStatus==1&&i1.pdgID==11) {
       momenta.push_back(ROOT::Math::PxPyPzMVector{i1.ps.x,i1.ps.y,i1.ps.z,i1.mass});
     }
     else {momenta.push_back(ROOT::Math::PxPyPzMVector{-1e10, -1e10, -1e10, -1e10});}
@@ -167,40 +131,6 @@ auto findGammaMC(const std::vector<dd4pod::Geant4ParticleData>& parts) {
     else {momenta.push_back(ROOT::Math::PxPyPzMVector{-1e10, -1e10, -1e10, -1e10});}
   }
   return momenta;
-}
-
-
-auto findVM_MC_match_REC(const std::vector<ROOT::Math::PxPyPzMVector> MC, 
-  const std::vector<ROOT::Math::PxPyPzMVector> REC)
-{
-  std::vector<ROOT::Math::PxPyPzMVector> vm_match;
-  for(auto& i1:MC){
-    auto v = ROOT::Math::PxPyPzMVector{-1e10, -1e10, -1e10, -1e10};
-    if(i1.Px()<-1e9) continue;
-    for(auto& i2:REC){
-      if(matchVectKine(i1,i2)&&fabs(i2.M()-i1.M())<vm_mass_width[which_vm]) v=i1;
-    }
-    vm_match.push_back(v);
-  }
-  return vm_match;
-}
-
-auto findVM_REC_NOT_match_MC(const std::vector<ROOT::Math::PxPyPzMVector> REC, 
-  const std::vector<ROOT::Math::PxPyPzMVector> MC)
-{
-  std::vector<ROOT::Math::PxPyPzMVector> vm_not_match;
-  for(auto& i1:REC){
-    auto v = ROOT::Math::PxPyPzMVector{-1e10, -1e10, -1e10, -1e10};
-    //here not cutting on rec mass yet.
-    bool VM_interested = true; 
-    if(fabs(i1.M()-vm_mass[which_vm])>vm_mass_width[which_vm]) VM_interested=false;
-    for(auto& i2:MC){
-      if(i2.Px()<-1e-9) continue;
-      if(!matchVectKine(i1,i2)&&VM_interested) v=i1;
-    }
-    vm_not_match.push_back(v);
-  }
-  return vm_not_match;
 }
 
 auto resolution_MC_match_REC(const std::vector<ROOT::Math::PxPyPzMVector> MC, 
@@ -248,22 +178,30 @@ auto getEta(const std::vector<ROOT::Math::PxPyPzMVector>& mom) {
 }
 
 
-auto giveme_t = [](std::vector<ROOT::Math::PxPyPzMVector> vm, 
-   std::vector<ROOT::Math::PxPyPzMVector> scatElec){
+auto giveme_t_MC(const std::vector<dd4pod::Geant4ParticleData>& parts){
   std::vector<double > t_vec;
-  for(auto& i2: scatElec){
-    for(auto& i1: vm){
-      if(fabs(i1.Rapidity())>3.0||fabs(i1.M()-vm_mass[which_vm])>vm_mass_width[which_vm]) continue;
-      if(i2.Px()<-1e9) continue;
-      // TLorentzVector eIn(0,0,-18,18);
-      // TLorentzVector eOut;eOut.SetPxPyPzE(i2.Px(),i2.Py(),i2.Pz(),i2.E());
-      // TLorentzVector vmOut;vmOut.SetPxPyPzE(i1.Px(),i1.Py(),i1.Pz(),i1.E());
-      // double method_E = (eIn-eOut-vmOut).Mag2();
-      // t_vec.push_back( -method_E );
-      TVector2 sum_pt(i1.Px()+i2.Px(), i1.Py()+i2.Py());
-      t_vec.push_back( sum_pt.Mod2() );
+  TLorentzVector pIn,pOut;
+  for(auto& i1 : parts){
+    if(i1.genStatus==4&&i1.pdgID==2212) {
+      TVector3 pIn_v3(i1.ps.x,i1.ps.y,i1.ps.z);
+      pIn.SetVectM(pIn_v3,i1.mass);
+    }
+    if(i1.genStatus==1&&i1.pdgID==2212) {
+      TVector3 pOut_v3(i1.ps.x,i1.ps.y,i1.ps.z);
+      pOut.SetVectM(pOut_v3,i1.mass);
     }
   }
+  t_vec.push_back( -(pOut-pIn).Mag2() );
   
   return t_vec;
-};
+}
+
+
+
+
+
+
+
+
+
+
