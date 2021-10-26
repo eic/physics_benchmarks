286dfacb9396c521e8560c1807d9775077710d6c
Alex Jentsch <ajentsch@bnl.gov>
2021-10-26 20:49:07 +0000

    Still trying to debug.

--- benchmarks/dvcs-d/analysis/pleaseIncludeMe.h
+++ benchmarks/dvcs-d/analysis/pleaseIncludeMe.h
@@ -167,17 +167,17 @@ auto findScatProton(const std::vector<eic::ReconstructedParticleData>& FF){
   std::vector<ROOT::Math::PxPyPzMVector> momenta;
   for(auto& i1 : FF){
     if(i1.charge==1){
-      //Double_t px = i1.p.x;
-      //Double_t py = i1.p.y;
-      //Double_t pz = i1.p.z;
-      //Double_t s = std::sin(0.025);
-      //Double_t c = std::cos(0.025);
-      //Double_t zz = pz;
-      //Double_t xx = px;
-      //pz = c*zz - s*xx;
-      //px = s*zz + c*xx;
-      //momenta.push_back(ROOT::Math::PxPyPzMVector{px,py,pz,i1.mass});
-      momenta.push_back(ROOT::Math::PxPyPzMVector{i1.p.x,i1.p.y,i1.p.z,i1.mass});
+      Double_t px = i1.p.x;
+      Double_t py = i1.p.y;
+      Double_t pz = i1.p.z;
+      Double_t s = std::sin(-0.025);
+      Double_t c = std::cos(-0.025);
+      Double_t zz = pz;
+      Double_t xx = px;
+      pz = c*zz - s*xx;
+      px = s*zz + c*xx;
+      momenta.push_back(ROOT::Math::PxPyPzMVector{px,py,pz,i1.mass});
+      //momenta.push_back(ROOT::Math::PxPyPzMVector{i1.p.x,i1.p.y,i1.p.z,i1.mass});
     }
   }
   return momenta;
