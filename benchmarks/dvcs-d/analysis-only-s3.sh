4795ba457b9cf9b78c0813e07d9fd340cc193512
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2021-10-29 15:31:24 -0400

    add analysis-only-s3 script

---
+++ benchmarks/dvcs-d/analysis-only-s3.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+## =============================================================================
+## Only read in the full simulations from s3 and do analysis
+## =============================================================================
+
+## make sure we launch this script from the project root directory
+PROJECT_ROOT="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"/../..
+pushd ${PROJECT_ROOT}
+
+FILE_NAME_TAG="dvcs-d"
+REC_FILE="dvcs-d_output.root"
+JUGGLER_DETECTOR="ATHENA"
+RESULTS_PATH=results/s3_full/dvcs-d
+BEAM_TAG="10X100"
+
+set INPUT_PATH_FROM_S3_TAG = "s3_full/${FILE_NAME_TAG}"
+echo "Running the dvcs-d analysis"
+
+DATA_URL="S3/eictest/ATHENA/ATHENA/RECO/acadia-v2.1/EXCLUSIVE/DVCS_ABCONV/10x100/DVCS.1.ab.hiAcc.10x100_novtx.0049.root"
+mc -C . config host add S3 https://dtn01.sdcc.bnl.gov:9000 $S3_ACCESS_KEY $S3_SECRET_KEY
+mc -C . cp  --insecure ${DATA_URL} "${INPUT_PATH_FROM_S3}/REC_FILE" 
+if [[ "$?" -ne "0" ]] ; then
+  echo "Failed to download hepmc file"
+  exit 1
+fi
+
+## =============================================================================
+## Step 4: Analysis
+## write a temporary configuration file for the analysis script
+echo "Running analysis"
+CONFIG="${INPUT_PATH_FROM_S3}/${FILE_NAME_TAG}.json"
+cat << EOF > ${CONFIG}
+{
+  "rec_file": "${REC_FILE}",
+  "detector": "${JUGGLER_DETECTOR}",
+  "output_prefix": "${RESULTS_PATH}",
+  "test_tag": "${BEAM_TAG}"
+}
+EOF
+#cat ${CONFIG}
+root -b -q "benchmarks/dvcs-d/analysis/dvcs_d_analysis.cxx+(\"${CONFIG}\")"
+if [[ "$?" -ne "0" ]] ; then
+  echo "ERROR running rec_dvcs-d_analysis script"
+  exit 1
+fi
+
+echo "DVCS-d benchmarks complete"
