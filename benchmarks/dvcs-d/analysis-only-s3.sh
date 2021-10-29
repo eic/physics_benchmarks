8177324cda538c68648fc3c6f4af39858b934e26
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2021-10-29 16:00:30 -0400

    add analysis-only-s3 script

--- benchmarks/dvcs-d/analysis-only-s3.sh
+++ benchmarks/dvcs-d/analysis-only-s3.sh
@@ -22,7 +22,7 @@ echo "Running the dvcs-d analysis"
 
 DATA_URL=S3/eictest/ATHENA/ATHENA/RECO/acadia-v2.1/EXCLUSIVE/DVCS_ABCONV/10x100/DVCS.1.ab.hiAcc.10x100_novtx.0049.root
 mc -C . config host add S3 https://dtn01.sdcc.bnl.gov:9000 $S3_ACCESS_KEY $S3_SECRET_KEY
-mc cp --insecure DATA_URL INPUT_PATH_FROM_S3/${REC_FILE}
+mc cp S3/eictest/ATHENA/ATHENA/RECO/acadia-v2.1/EXCLUSIVE/DVCS_ABCONV/10x100/DVCS.1.ab.hiAcc.10x100_novtx.0049.root  s3_full/dvcs-d/dvcs-d_output.root
 # if [[ "$?" -ne "0" ]] ; then
 #   echo "Failed to download hepmc file"
 #   exit 1
