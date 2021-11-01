207af4255eadfc9c8ff946a781797705b5a01342
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2021-11-01 15:07:53 -0400

    try new samples

--- benchmarks/diffractive_phi/diffractive_phi.sh
+++ benchmarks/diffractive_phi/diffractive_phi.sh
@@ -71,10 +71,7 @@ set -- "${POSITIONAL[@]}" # restore positional parameters
 print_env.sh
 
 FILE_NAME_TAG="diffractive_phi"
-# DATA_URL="S3/eictest/ATHENA/EVGEN/EXCLUSIVE/DIFFRACTIVE_PHI_ABCONV/Sartre/sartre_bnonsat_Au_phi_ab_eAu_1.hepmc"
-DATA_URL="/gpfs02/eic/ztu/ATHENA/detectorSimulations/BeAGLE/hepmc3_prod_final/Output_input_temp_1.hepmc"
-# DATA_URL="/gpfs02/eic/ztu/ATHENA/detectorSimulations/Sartre/hepmc3_test_Oct_5/sartre_bnonsat_Au_phi_8_noNuclei.hepmc"
-# DATA_URL="/gpfs02/eic/ztu/ATHENA/detectorSimulations/eSTARLight/phiineA50k.hepmc"
+DATA_URL="S3/eictest/ATHENA/EVGEN/EXCLUSIVE/DIFFRACTIVE_PHI_ABCONV/Sartre/sartre_bnonsat_Au_phi_ab_eAu_1.hepmc"
 
 
 mkdir -p "${LOCAL_DATA_PATH}/input/${FILE_NAME_TAG}"
