12411940b41d1bf8535b1e64d20a2f439875cb63
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2021-11-02 14:20:35 -0400

    change to neutron and test 10k events

--- benchmarks/dvcs-d/gen.sh
+++ benchmarks/dvcs-d/gen.sh
@@ -75,7 +75,7 @@ echo "Generator output for $GEN_TAG not found in cache, need to copy generator f
 #   exit 1
 # fi
 
-DATA_URL="S3/eictest/ATHENA/EVGEN/EXCLUSIVE/DVCSD_ABCONV/10x100/DVCS-eD-ab-hiAcc-10x100.hepmc"
+DATA_URL="S3/eictest/ATHENA/EVGEN/EXCLUSIVE/DVCSD_ABCONV/10x100/DVCS-eD-ab-hiAcc-10x100-2.hepmc"
 mc -C . config host add S3 https://dtn01.sdcc.bnl.gov:9000 $S3_ACCESS_KEY $S3_SECRET_KEY
 mc -C . cat  --insecure ${DATA_URL} > "${TMP_PATH}/${GEN_TAG}.hepmc"
 if [[ "$?" -ne "0" ]] ; then
