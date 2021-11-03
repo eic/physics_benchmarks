de0a40cb0167a2d75b892d158a2b983611a1c25a
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2021-11-03 15:23:29 -0400

    Try to find the wrong events

--- benchmarks/dvcs-d/reco_local.sh
+++ benchmarks/dvcs-d/reco_local.sh
@@ -62,8 +62,10 @@ PLOT_TAG=${CONFIG}
 echo "Running Geant4 simulation"
 npsim --runType batch \
       --part.minimalKineticEnergy 1000*GeV  \
-      -v WARNING \
+      -v INFO \
+      --skipNEvents 3000 \
       --numberOfEvents ${JUGGLER_N_EVENTS} \
+      --output.part DEBUG \
       --compactFile ${DETECTOR_PATH}/${JUGGLER_DETECTOR}.xml \
       --inputFiles ${GEN_FILE} \
       --outputFile ${SIM_FILE}
