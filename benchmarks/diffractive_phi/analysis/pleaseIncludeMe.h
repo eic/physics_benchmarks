ff22536ecdb55be7a32955b346e7694679107699
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2021-11-09 12:15:57 -0500

    t method E

--- benchmarks/diffractive_phi/analysis/pleaseIncludeMe.h
+++ benchmarks/diffractive_phi/analysis/pleaseIncludeMe.h
@@ -358,13 +358,13 @@ auto giveme_t = [](std::vector<ROOT::Math::PxPyPzMVector> vm,
     for(auto& i1: vm){
       if(fabs(i1.Rapidity())>3.0||fabs(i1.M()-vm_mass[which_vm])>vm_mass_width[which_vm]) continue;
       if(i2.Px()<-1e9) continue;
-      // TLorentzVector eIn(0,0,-18,18);
-      // TLorentzVector eOut;eOut.SetPxPyPzE(i2.Px(),i2.Py(),i2.Pz(),i2.E());
-      // TLorentzVector vmOut;vmOut.SetPxPyPzE(i1.Px(),i1.Py(),i1.Pz(),i1.E());
-      // double method_E = (eIn-eOut-vmOut).Mag2();
-      // t_vec.push_back( -method_E );
-      TVector2 sum_pt(i1.Px()+i2.Px(), i1.Py()+i2.Py());
-      t_vec.push_back( sum_pt.Mod2() );
+      TLorentzVector eIn(0,0,-18,18);
+      TLorentzVector eOut;eOut.SetPxPyPzE(i2.Px(),i2.Py(),i2.Pz(),i2.E());
+      TLorentzVector vmOut;vmOut.SetPxPyPzE(i1.Px(),i1.Py(),i1.Pz(),i1.E());
+      double method_E = (eIn-eOut-vmOut).Mag2();
+      t_vec.push_back( -method_E );
+      // TVector2 sum_pt(i1.Px()+i2.Px(), i1.Py()+i2.Py());
+      // t_vec.push_back( sum_pt.Mod2() );
     }
   }
   
