388787af39d22c143d9fa070a100cbd76aeca8fd
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2021-11-09 19:29:23 -0500

    t method E

--- benchmarks/diffractive_phi/analysis/pleaseIncludeMe.h
+++ benchmarks/diffractive_phi/analysis/pleaseIncludeMe.h
@@ -379,7 +379,28 @@ auto giveme_t_E = [](std::vector<ROOT::Math::PxPyPzMVector> vm,
       TLorentzVector eOut;eOut.SetPxPyPzE(i2.Px(),i2.Py(),i2.Pz(),i2.E());
       TLorentzVector vmOut;vmOut.SetPxPyPzE(i1.Px(),i1.Py(),i1.Pz(),i1.E());
       //exact method need no boost.
-      double method_E = (eIn-eOut-vmOut).Mag2();
+      //reverse engineering.boost
+      TVector3 boost_to_cm = (pIn+eIn).BoostVector();
+      double angle_x = TMath::ATan(boost_to_cm.Z() / boost_to_cm.X());
+      double angle_y = TMath::ATan(boost_to_cm.Z() / boost_to_cm.Y());
+      vmOut.Boost(-boost_to_cm);
+      vmOut.RotateY(-angle_x);
+      vmOut.RotateX(-angle_y);
+
+      eIn.Boost(-boost_to_cm);
+      eIn.RotateY(-angle_x);
+      eIn.RotateX(-angle_y);
+
+      eOut.Boost(-boost_to_cm);
+      eOut.RotateY(-angle_x);
+      eOut.RotateX(-angle_y);
+      
+      TVector3 boost_to_lab = (pInTrue+eInTrue).BoostVector();
+      vmOut.Boost(boost_to_lab);
+      eIn.Boost(boost_to_lab);
+      eOut.Boost(boost_to_lab);
+      //end
+      double method_E = (eInTrue-eOut-vmOut).Mag2();
       t_vec.push_back( -method_E );
     }
   }
