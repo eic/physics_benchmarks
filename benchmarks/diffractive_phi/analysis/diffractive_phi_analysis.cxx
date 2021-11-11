3b3275b1824a1a2d30c30a338164f2a384c7e5b0
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2021-11-10 23:34:38 -0500

    single track quantity

--- benchmarks/diffractive_phi/analysis/diffractive_phi_analysis.cxx
+++ benchmarks/diffractive_phi/analysis/diffractive_phi_analysis.cxx
@@ -161,7 +161,7 @@ int diffractive_phi_analysis(const std::string& config_name, const int vm_type=1
              .Define("vm_res_pt",resolution_MC_match_REC,{"VMMC","vm"})
              .Define("protonMC",findScatProtonMC,{"mcparticles"}).Define("proton_mc_p",getP,{"protonMC"})
              .Define("VMMC_daugPlus",findVM_DaugPlus_MC,{"mcparticles"}).Define("ptVMMC_daugPlus",getPt,{"VMMC_daugPlus"})
-             .Define("p1_rec_pt",getPt,{"p1"}).Define("p1_res_pt",resolution_MC_match_REC,{"ptVMMC_daugPlus","p1_rec_pt"})
+             .Define("p1_rec_pt",getPt,{"p1"}).Define("p1_res_pt",resolution_MC_match_REC,{"VMMC_daugPlus","p1"})
              .Filter(kineCut,{"Q2_elec","y_elec"});
 
   auto h_Pt_VM_MC_total = d4.Histo1D({"h_Pt_VM_MC_total", "; GeV; counts", 50, 0, 2}, "vm_mc_pt");
