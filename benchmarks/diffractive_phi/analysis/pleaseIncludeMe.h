7f1d58703dc2bff49287048e0d5d34a3910ecb04
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2021-11-15 10:03:50 -0500

    hand smear to full simulations number

--- benchmarks/diffractive_phi/analysis/pleaseIncludeMe.h
+++ benchmarks/diffractive_phi/analysis/pleaseIncludeMe.h
@@ -486,11 +486,11 @@ auto giveme_t_A = [](std::vector<ROOT::Math::PxPyPzMVector> vm,
       vmOut = vmOut_MC;
       eOut = eOut_MC;
 
-      double e_px_res=gRandom->Gaus(0.0,0.0147);
+      double e_px_res=gRandom->Gaus(0.0,0.0035);
       double e_px = eOut.Px()*(1.+e_px_res);
       eOut.SetPx(e_px);
 
-      double e_py_res=gRandom->Gaus(0.0,0.0147);
+      double e_py_res=gRandom->Gaus(0.0,0.0035);
       double e_py = eOut.Py()*(1.+e_py_res);
       eOut.SetPy(e_py);
 
