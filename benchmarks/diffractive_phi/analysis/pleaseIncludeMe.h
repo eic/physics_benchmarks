932346aab5e915debdda50f08a8470859be97dce
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2021-11-18 14:20:28 -0500

    try Q2>1 back to standard

--- benchmarks/diffractive_phi/analysis/pleaseIncludeMe.h
+++ benchmarks/diffractive_phi/analysis/pleaseIncludeMe.h
@@ -516,11 +516,13 @@ auto giveme_t_L = [](std::vector<ROOT::Math::PxPyPzMVector> vm,
   const std::vector<dd4pod::Geant4ParticleData>& mc){
 
   TLorentzVector eIn(0,0,-18,18);
-  TLorentzVector eInTrue(0,0,-18,18);
-  TLorentzVector pInTrue(0,0,109.996,110.000);
   TLorentzVector pIn(-2.749,0,109.996,110.034);
   TLorentzVector vmOut_MC, eOut_MC;
   for(auto& i3 : mc){
+    if(i3.genStatus==4&&i3.pdgID==11){
+      TVector3 eInv3(i3.ps.x,i3.ps.y,i3.ps.z);
+      eIn.SetVectM(eInv3,i3.mass);
+    }
      if(i3.genStatus==1&&i3.pdgID==11){
       TVector3 eOutv3(i3.ps.x,i3.ps.y,i3.ps.z);
       eOut_MC.SetVectM(eOutv3,i3.mass);
@@ -539,16 +541,16 @@ auto giveme_t_L = [](std::vector<ROOT::Math::PxPyPzMVector> vm,
       TLorentzVector vmOut;vmOut.SetPxPyPzE(i1.Px(),i1.Py(),i1.Pz(),i1.E());
       TLorentzVector aInVec(pIn.Px()*197,pIn.Py()*197,pIn.Pz()*197,sqrt(pIn.Px()*197*pIn.Px()*197 + pIn.Py()*197*pIn.Py()*197 + pIn.Pz()*197*pIn.Pz()*197 + MASS_AU197*MASS_AU197) );
       
-      // vmOut = vmOut_MC;
-      // eOut = eOut_MC;
+      vmOut = vmOut_MC;
+      eOut = eOut_MC;
 
-      // double e_pt_res=gRandom->Gaus(0.0,0.012);//gaus fit~0.0068, set this number by full simulations for Q2>15
-      // double e_pt = eOut.Pt()*(1.+e_pt_res);
-      // eOut.SetPerp(e_pt);
+      double e_pt_res=gRandom->Gaus(0.0,0.012);//gaus fit~0.0068, set this number by full simulations for Q2>15
+      double e_pt = eOut.Pt()*(1.+e_pt_res);
+      eOut.SetPerp(e_pt);
 
-      // double vm_pt_res=gRandom->Gaus(0.0,0.015);//gaus fit~0.0065, set this number by full simulations for Q2>15
-      // double vm_pt = vmOut.Pt()*(1.+vm_pt_res);
-      // vmOut.SetPerp(vm_pt);
+      double vm_pt_res=gRandom->Gaus(0.0,0.015);//gaus fit~0.0065, set this number by full simulations for Q2>15
+      double vm_pt = vmOut.Pt()*(1.+vm_pt_res);
+      vmOut.SetPerp(vm_pt);
 
       double method_L = -99.;
       TLorentzVector a_beam_scattered = aInVec-(vmOut+eOut-eIn);
