cb849ded48070dc92dda0592709985ffc3350d2e
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2022-01-04 14:52:40 -0500

    change to jpsi test run

--- benchmarks/diffractive_phi/analysis-only.sh
+++ benchmarks/diffractive_phi/analysis-only.sh
@@ -110,7 +110,7 @@ cat << EOF > ${CONFIG}
 }
 EOF
 #cat ${CONFIG}
-export VM_TYPE_TAG=1
+export VM_TYPE_TAG=2
 export MC_TYPE_TAG=1
 root -b -q "benchmarks/diffractive_phi/analysis/diffractive_phi_analysis.cxx+(\"${CONFIG}\",${VM_TYPE_TAG},${MC_TYPE_TAG})"
 #root -b -q "benchmarks/diffractive_phi/analysis/test_phi_analysis.cxx+(\"${CONFIG}\",${VM_TYPE_TAG},${MC_TYPE_TAG})"
