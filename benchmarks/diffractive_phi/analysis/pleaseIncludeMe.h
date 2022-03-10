b7318ec3e90e0811c197d6d5d5d53f7d42ab8afa
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2022-03-10 12:50:06 -0500

    change of include name?

--- benchmarks/diffractive_phi/analysis/pleaseIncludeMe.h
+++ benchmarks/diffractive_phi/analysis/pleaseIncludeMe.h
@@ -26,8 +26,8 @@
 #include "fmt/core.h"
 
 #include "nlohmann/json.hpp"
-#include "eicd/InclusiveKinematicsData.h"
-#include "eicd/ReconstructedParticleData.h"
+#include "eic/InclusiveKinematicsData.h"
+#include "eic/ReconstructedParticleData.h"
 
 #define PI            3.1415926
 #define MASS_ELECTRON 0.00051
