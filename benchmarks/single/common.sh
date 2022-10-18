fd8d1f374de510b526642d0f9aba9dfe2557efef
Wouter Deconinck <wdconinc@gmail.com>
2022-10-18 23:58:56 +0000

    fix: use base and tree file that agree

--- benchmarks/single/common.sh
+++ benchmarks/single/common.sh
@@ -5,4 +5,5 @@ fi
 export JUGGLER_FILE_NAME_TAG="${1:-e-_1GeV_45to135deg}"
 export JUGGLER_GEN_FILE="benchmarks/single/${JUGGLER_FILE_NAME_TAG}.steer"
 export JUGGLER_SIM_FILE="sim_output/sim_${JUGGLER_FILE_NAME_TAG}.edm4hep.root"
-export JUGGLER_REC_FILE="sim_output/rec_${JUGGLER_FILE_NAME_TAG}.root"
+export JUGGLER_REC_FILE_BASE="sim_output/rec_${JUGGLER_FILE_NAME_TAG}"
+export JUGGLER_REC_FILE="${JUGGLER_REC_FILE_BASE}.tree.edm4eic.root"
