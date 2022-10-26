d0a263c0d128b8c3c65f707035c4491a33cc52d6
Wouter Deconinck <wdconinc@gmail.com>
2022-10-26 18:18:43 +0000

    feat: single:reconstruct rootls

--- benchmarks/single/reconstruct.sh
+++ benchmarks/single/reconstruct.sh
@@ -4,8 +4,9 @@ source $(dirname $0)/common.sh $*
 
 # Reconstruct
 run_eicrecon_reco_flags.py ${JUGGLER_SIM_FILE} ${JUGGLER_REC_FILE_BASE}
-
 if [ "$?" -ne "0" ] ; then
   echo "ERROR running eicrecon"
   exit 1
 fi
+
+rootls -t ${JUGGLER_REC_FILE_BASE}.tree.edm4eic.root
