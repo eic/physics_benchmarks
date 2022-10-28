62e00f42ca8f17be1712be9ba6b562006235a415
Wouter Deconinck <wdconinc@gmail.com>
2022-10-28 18:55:42 +0000

    fix: re-enable barrel hcal clustering

--- options/reconstruction.py
+++ options/reconstruction.py
@@ -777,6 +777,7 @@ cb_hcal_reco = CalHitReco(
     thresholdFactor=5.0,
     samplingFraction=cb_hcal_sf,
     readoutClass="HcalBarrelHits",
+    sectorField="sector",
     **cb_hcal_daq,
 )
 algorithms.append(cb_hcal_reco)
@@ -786,10 +787,10 @@ cb_hcal_merger = CalHitsMerger(
     inputHitCollection=cb_hcal_reco.outputHitCollection,
     outputHitCollection="HcalBarrelMergedHits",
     readoutClass="HcalBarrelHits",
-    fields=["tower"],
+    fields=["tile"],
     fieldRefNumbers=[0],
 )
-#algorithms.append(cb_hcal_merger)
+algorithms.append(cb_hcal_merger)
 
 cb_hcal_cl = IslandCluster(
     "cb_hcal_cl",
@@ -799,7 +800,7 @@ cb_hcal_cl = IslandCluster(
     minClusterCenterEdep=30.0 * MeV,
     localDistXY=[15.0 * cm, 15.0 * cm],
 )
-#algorithms.append(cb_hcal_cl)
+algorithms.append(cb_hcal_cl)
 
 cb_hcal_clreco = RecoCoG(
     "cb_hcal_clreco",
@@ -809,7 +810,7 @@ cb_hcal_clreco = RecoCoG(
     outputAssociations="HcalBarrelClustersAssoc",
     logWeightBase=6.2,
 )
-#algorithms.append(cb_hcal_clreco)
+algorithms.append(cb_hcal_clreco)
 
 # Hcal Hadron Endcap
 ci_hcal_daq = calo_daq["hcal_pos_endcap"]
@@ -1078,7 +1079,7 @@ match_clusters = MatchClusters(
         str(barrel_clus_merger.outputClusters),
         str(ci_ecal_clmerger.outputClusters),
         str(ce_hcal_clreco.outputClusterCollection),
-        #str(cb_hcal_clreco.outputClusterCollection),
+        str(cb_hcal_clreco.outputClusterCollection),
         str(ci_hcal_clreco.outputClusterCollection),
     ],
     inputClustersAssoc=[
@@ -1086,7 +1087,7 @@ match_clusters = MatchClusters(
         str(barrel_clus_merger.outputAssociations),
         str(ci_ecal_clmerger.outputAssociations),
         str(ce_hcal_clreco.outputAssociations),
-        #str(cb_hcal_clreco.outputAssociations),
+        str(cb_hcal_clreco.outputAssociations),
         str(ci_hcal_clreco.outputAssociations),
     ],
     outputParticles="ReconstructedParticles",
