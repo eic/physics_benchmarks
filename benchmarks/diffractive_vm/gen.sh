f91449c2c1bec9ec32f770bc8610e8987bbe1f5e
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2022-11-10 20:48:37 -0500

    update dis

--- benchmarks/diffractive_vm/gen.sh
+++ benchmarks/diffractive_vm/gen.sh
@@ -53,16 +53,21 @@ echo "Generator output for $GEN_TAG not found in cache, need to copy generator f
 
 ## =============================================================================
 ## Step 4: Copy the event generator file over
+# Exclusive Jpsi event sample
 # DATA_URL="S3/eictest/ATHENA/EVGEN/EXCLUSIVE/DIFFRACTIVE_PHI_ABCONV/Sartre/Coherent/sartre_bnonsat_Au_phi_ab_eAu_1.hepmc"
-# DIS event
-DATA_URL="S3/eictest/EPIC/EVGEN/DIS/NC/18x275/minQ2=1/pythia8NCDIS_18x275_minQ2=1_beamEffects_xAngle=-0.025_hiDiv_1.hepmc"
+# DIS event sample
+#DATA_URL="S3/eictest/EPIC/EVGEN/DIS/NC/18x275/minQ2=1/pythia8NCDIS_18x275_minQ2=1_beamEffects_xAngle=-0.025_hiDiv_1.hepmc"
 mc -C . config host add S3 https://dtn01.sdcc.bnl.gov:9000 $S3_ACCESS_KEY $S3_SECRET_KEY
 # mc -C . cat  --insecure ${DATA_URL} | awk -F '\\@' '{print $1}' > "${TMP_PATH}/${GEN_TAG}.hepmc"
-mc cp --insecure ${DATA_URL} ${TMP_PATH}/${GEN_TAG}.hepmc
-if [[ "$?" -ne "0" ]] ; then
-  echo "Failed to download hepmc file"
-  exit 1
-fi
+# DATA_URL="S3/eictest/EPIC/EVGEN/DIS/NC/18x275/minQ2=1/pythia8NCDIS_18x275_minQ2=1_beamEffects_xAngle=-0.025_hiDiv_vtxfix_1_000.hepmc.gz"
+for i in {0..10}
+do
+  mc cp --insecure "S3/eictest/EPIC/EVGEN/DIS/NC/18x275/minQ2=1/pythia8NCDIS_18x275_minQ2=1_beamEffects_xAngle=-0.025_hiDiv_vtxfix_1_00${i}.hepmc.gz" ${TMP_PATH}/${GEN_TAG}.hepmc
+  if [[ "$?" -ne "0" ]] ; then
+    echo "Failed to download hepmc file"
+    exit 1
+  fi
+done
 
 ## =============================================================================
 ## Step 5: Finally, move relevant output into the artifacts directory and clean up
