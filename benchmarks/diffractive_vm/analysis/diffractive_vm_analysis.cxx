c1c64ee59332fc3b3272e2ba2845979eb701d519
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2022-11-22 11:15:00 -0500

    update analysis code

--- benchmarks/diffractive_vm/analysis/diffractive_vm_analysis.cxx
+++ benchmarks/diffractive_vm/analysis/diffractive_vm_analysis.cxx
@@ -212,7 +212,7 @@ int diffractive_vm_analysis(const std::string& config_name, const int vm_type=1,
              .Define("w_elec", "InclusiveKinematicsElectron.W")
              .Define("p1", momenta_from_reconstruction_plus, {"ReconstructedParticles"})
              .Define("p2", momenta_from_reconstruction_minus, {"ReconstructedParticles"})
-             .Define("scatElec","InclusiveKinematicsElectron.scat")
+             .Define("scatElec",findScatElec,{"ReconstructedParticles","MCParticles"})
              .Define("vm", vector_sum, {"p1","p2"})
              .Define("t_rec", giveme_t_L, {"vm","scatElec","MCParticles"})
              .Filter(eventVetoCut,{"ReconstructedFFParticles","ReconstructedParticles"})
