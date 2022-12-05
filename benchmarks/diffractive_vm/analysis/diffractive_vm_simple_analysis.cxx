11e1a76f589d69f315066eb56231e00b1f1c5651
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2022-12-05 09:34:23 -0500

    try a simple analysis

--- benchmarks/diffractive_vm/analysis/diffractive_vm_simple_analysis.cxx
+++ benchmarks/diffractive_vm/analysis/diffractive_vm_simple_analysis.cxx
@@ -1,28 +1,12 @@
 #include "pleaseIncludeMe.h"
-int diffractive_vm_simple_analysis(const std::string& config_name, const int vm_type=1, const int mc_type=0)
+int diffractive_vm_simple_analysis(TString config_name)
 {
 	// read our configuration
-	std::ifstream  config_file{config_name};
-	nlohmann::json config;
-	config_file >> config;
+	TString name_for_input = "/gpfs02/eic/ztu/EPIC/physics/Simulation_Campaign_Oct2022/physics_benchmarks/local_data/tmp/18on110/rec-" + config_name + ".root";
 
-	const std::string rec_file      = config["rec_file"];
-	const std::string detector      = config["detector"];
-	const std::string output_prefix = config["output_prefix"];
-	const std::string test_tag      = config["test_tag"];
-
-	fmt::print(fmt::emphasis::bold | fg(fmt::color::forest_green),
-	         "Running diffractive phi analysis...\n");
-	fmt::print(" - Detector package: {}\n", detector);
-	fmt::print(" - input file: {}\n", rec_file);
-	fmt::print(" - output prefix: {}\n", output_prefix);
-	fmt::print(" - test tag: {}\n", test_tag);
-
-	TString tempname = (TString) config_name;
-
-	auto file=new TFile(tempname);
+	auto file=new TFile(name_for_input);
 	auto tree = (TTree *) file->Get("events");
-	
+
 	tree->Draw("MCParticles.momentum.z");
 
 
