00921e9ce0e5c7b45c5fd96c70444b4e226de5b0
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2022-12-05 15:18:32 -0500

    try a simple analysis

--- benchmarks/diffractive_vm/analysis/diffractive_vm_simple_analysis.cxx
+++ benchmarks/diffractive_vm/analysis/diffractive_vm_simple_analysis.cxx
@@ -19,6 +19,8 @@ int diffractive_vm_simple_analysis(const std::string& config_name)
 	auto tree = (TTree *) file->Get("events");
     TTreeReader tree_reader(tree);       // !the tree reader
     
+    TTreeReaderArray<int> mc_genStatus_array = {tree_reader, "MCParticles.generatorStatus"};
+
     // MC particle pz array for each MC particle
     TTreeReaderArray<float> mc_px_array = {tree_reader, "MCParticles.momentum.x"};
     TTreeReaderArray<float> mc_py_array = {tree_reader, "MCParticles.momentum.y"};
@@ -45,14 +47,33 @@ int diffractive_vm_simple_analysis(const std::string& config_name)
     TString output_name_dir = output_prefix.c_str();
   	TFile* output = new TFile(output_name_dir+"_output.root","RECREATE");
 
-    TH1D* h_eta = new TH1D("h_eta",";#eta",100,-5,5);
-    TH1D* h_energy_MC = new TH1D("h_energy_MC",";E_{MC} (GeV)",100,0,20);
+  	//events
+    TH1D* h_Q2_e = new TH1D("h_Q2_e",";#eta",100,0,20);
+    TH1D* h_y_e = new TH1D("h_y_e",";#eta",100,0,1);
+ 	TH1D* h_energy_MC = new TH1D("h_energy_MC",";E_{MC} (GeV)",100,0,20);
+    TH1D* h_t_MC = new TH1D("h_t_MC",";t; counts",100,0,0.2);
+
     TH1D* h_energy_REC = new TH1D("h_energy_REC",";E_{REC} (GeV)",100,0,20);
+    
+    //track
+    TH1D* h_eta = new TH1D("h_eta",";#eta",100,-5,5);
+   	
+   	//energy clus
     TH2D* h_emClus_position_REC = new TH2D("h_emClus_position_REC",";x (cm);y (cm)",400,-800,800,400,-800,800);
     TH2D* h_energy_res = new TH2D("h_energy_res",";E_{MC} (GeV); E_{MC}-E_{REC}/E_{MC}",100,0,20,1000,-1,1);
 
 	tree_reader.SetEntriesRange(0, tree->GetEntries());
     while (tree_reader.Next()) {
+
+		/*
+	    Beam particles
+	    */
+    	TLorentzVector ebeam(0,0,0,0);
+    	TLorentzVector pbeam(0,0,0,0);
+
+    	TLorentzVector vmMC(0,0,0,0);
+    	TLorentzVector kplusMC(0,0,0,0);
+    	TLorentzVector kminusMC(0,0,0,0);
 
     	//MCParticles
     	TLorentzVector scatMC(0,0,0,0);
@@ -60,22 +81,43 @@ int diffractive_vm_simple_analysis(const std::string& config_name)
     	double maxPt=-99.;
     	for(int imc=0;imc<mc_px_array.GetSize();imc++){
     		TVector3 mctrk(mc_px_array[imc],mc_py_array[imc],mc_pz_array[imc]);	
+    		if(mc_genStatus_array[imc]==4){
+    			if(mc_pdg_array[imc]==11) ebeam.SetVectM(mctrk, MASS_ELECTRON);
+  				if(mc_pdg_array[imc]==2212) pbeam.SetVectM(mctrk, MASS_PROTON);
+    		}
     		if(mc_pdg_array[imc]==11 	
     			&& mctrk.Perp()>maxPt){
     			maxPt=mctrk.Perp();
     			mc_elect_index=imc;
     			scatMC.SetVectM(mctrk,mc_mass_array[imc]);
     		}
+    		if(mc_pdg_array[imc]==321
+    			&& mc_genStatus_array[imc]==1) kplusMC.SetVectM(mctrk,MASS_KAON);
+			if(mc_pdg_array[imc]==-321
+    			&& mc_genStatus_array[imc]==1) kminusMC.SetVectM(mctrk,MASS_KAON);
+
     	}
+    	vmMC=kplusMC+kminusMC;
+    	//protection.
+    	if(ebeam.E()==pbeam.E() && ebeam.E()==0) {
+    		std::cout << "problem with MC incoming beams" << std::endl;
+    		continue;
+    	}
+    	TLorentzVector qbeam=ebeam-scatMC;
+    	double Q2=-(qbeam).Mag2()  
+		double pq=pbeam.Dot(qbeam);
+		double y= pq/pbeam.Dot(ebeam);
+		if(Q2<1.||Q2>10.) continue;
+		if(y<0.01||y>0.95) continue;
+
+		h_Q2_e->Fill(Q2);
+		h_y_e->Fill(y);
     	h_energy_MC->Fill(scatMC.E());
 
-    	//association to mc;
-    	// int rec_cluster_id=-1;
-    	// for(int iasso=0;iasso<em_rec_id_array.GetSize();iasso++){
-    	// 	if(em_sim_id_array[iasso]==mc_elect_index){
-    	// 		rec_cluster_id = em_rec_id_array[iasso];
-    	// 	}
-    	// }
+    	if(vmMC.E()!=0){
+    		double method_E = (qbeam-vmOut).Mag2();
+    		h_t_MC->Fill( method_E);
+    	}
 
     	double maxEnergy=-99.;
     	double xpos=-999.;
