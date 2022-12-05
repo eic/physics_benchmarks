906d7d9a8009bfb234cfc66944bc7434bf7a3377
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2022-12-05 10:18:47 -0500

    try a simple analysis

--- benchmarks/diffractive_vm/analysis/diffractive_vm_simple_analysis.cxx
+++ benchmarks/diffractive_vm/analysis/diffractive_vm_simple_analysis.cxx
@@ -12,10 +12,10 @@ int diffractive_vm_simple_analysis(const std::string& config_name)
 	const std::string output_prefix = config["output_prefix"];
 	const std::string test_tag      = config["test_tag"];
 
-	TString name_of_input=(TString) rec_file;
+	TString name_of_input = (TString) rec_file;
 	std::cout << "what is this rec_file = " << name_of_input << endl;
 	
-	auto file=new TFile("/gpfs02/eic/ztu/EPIC/physics/Simulation_Campaign_Oct2022/physics_benchmarks/local_data/tmp/18on110/rec-batch_5_982.tree.edm4eic.root");
+	auto file=new TFile(name_of_input);
 	auto tree = (TTree *) file->Get("events");
     TTreeReader tree_reader(tree);       // !the tree reader
     tree->Print();
