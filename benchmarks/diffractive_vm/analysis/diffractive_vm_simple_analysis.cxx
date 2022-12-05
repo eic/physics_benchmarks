927f86b1aca9704d4fb0cf626496eb3f92a07086
Kong Tu <kong@zhoudunming-lt.star.bnl.gov>
2022-12-05 10:01:34 -0500

    try a simple analysis

--- benchmarks/diffractive_vm/analysis/diffractive_vm_simple_analysis.cxx
+++ benchmarks/diffractive_vm/analysis/diffractive_vm_simple_analysis.cxx
@@ -20,6 +20,8 @@ int diffractive_vm_simple_analysis(TString config_name)
     tree->Print();
 
     // Reconstructed particles pz array for each reconstructed particle
+    TTreeReaderArray<float> reco_px_array = {tree_reader, "ReconstructedChargedParticles.momentum.x"};
+    TTreeReaderArray<float> reco_py_array = {tree_reader, "ReconstructedChargedParticles.momentum.y"};
     TTreeReaderArray<float> reco_pz_array = {tree_reader, "ReconstructedChargedParticles.momentum.z"};
 
     // MC particle pz array for each MC particle
@@ -28,26 +30,19 @@ int diffractive_vm_simple_analysis(TString config_name)
     TTreeReaderArray<unsigned int> rec_id = {tree_reader, "ReconstructedChargedParticlesAssociations.recID"};
     TTreeReaderArray<unsigned int> sim_id = {tree_reader, "ReconstructedChargedParticlesAssociations.simID"};
 
+    TFile* output=new TFile("rec-"+config_name+"simple-output.root");
+    TH1D* h_eta = new TH1D("h_eta",";#eta",100,-5,5);
+
 	tree_reader.SetEntriesRange(0, tree->GetEntries());
     while (tree_reader.Next()) {
 
-        // Number of mc particles, reco particles and associations may differ
-        fmt::print("New event. N reco particles: {}, N mc particles: {}, N assoc: {}\n",
-                   reco_pz_array.GetSize(), mc_pz_array.GetSize(), rec_id.GetSize());
-
-        // Iterate over associations
-        for(unsigned int i=0; i < rec_id.GetSize(); i++) {
-
-            // For each association pull index of reco and MC array
-            auto reco_array_index = rec_id[i];
-            auto mc_array_index = sim_id[i];
-
-            float reco_pz = reco_pz_array[reco_array_index];
-            float mc_pz = mc_pz_array[mc_array_index];
-            fmt::print("   reco={:>10.4f} mc={:>10.4f}\n", reco_pz, mc_pz);
-        }
+    	for(int itrack=0;itrk<reco_pz_array.size();itrack++){
+    		TVector3 trk(reco_px_array[itrack],reco_py_array[itrack],reco_pz_array[itrack]);
+    		h_eta->Fill(trk.Eta());
+    	}
     }
-	
+	output->Write();
+	output->Close();
 
 	return 0;
 }
