8e90f5d4176c9313a195d6ce32f54d20c7398490
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2023-07-23 15:33:10 -0400

    ParticlesAssociations -> ParticleAssociations

--- benchmarks/diffractive_vmp/analysis/diffractive_vm.cxx
+++ benchmarks/diffractive_vmp/analysis/diffractive_vm.cxx
@@ -105,8 +105,8 @@ TTreeReaderArray<float> reco_py_array = {tree_reader, "ReconstructedChargedParti
 TTreeReaderArray<float> reco_pz_array = {tree_reader, "ReconstructedChargedParticles.momentum.z"};
 TTreeReaderArray<float> reco_charge_array = {tree_reader, "ReconstructedChargedParticles.charge"};
 
-TTreeReaderArray<unsigned int> rec_id = {tree_reader, "ReconstructedChargedParticlesAssociations.recID"};
-TTreeReaderArray<unsigned int> sim_id = {tree_reader, "ReconstructedChargedParticlesAssociations.simID"};
+TTreeReaderArray<unsigned int> rec_id = {tree_reader, "ReconstructedChargedParticleAssociations.recID"};
+TTreeReaderArray<unsigned int> sim_id = {tree_reader, "ReconstructedChargedParticleAssociations.simID"};
 
 std::string output_file = output_prefix + "/output.root";
 TFile* output = new TFile(output_file.c_str(), "RECREATE");
