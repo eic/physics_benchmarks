b2c8cd91f6ebf2a4690b1538e593d4e6d0d67711
Zachary Sweger <zsweger@berkeley.edu>
2024-05-17 15:57:55 -0700

    Create reconstruct.sh

---
+++ benchmarks/u_rho/reconstruct.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+source strict-mode.sh
+
+source benchmarks/u_rho/setup.config $*
+
+# Reconstruct
+if [ ${RECO} == "eicrecon" ] ; then
+  eicrecon ${JUGGLER_SIM_FILE} -Ppodio:output_file=${JUGGLER_REC_FILE}
+  if [[ "$?" -ne "0" ]] ; then
+    echo "ERROR running eicrecon"
+    exit 1
+  fi
+fi
+
+if [[ ${RECO} == "juggler" ]] ; then
+  gaudirun.py options/reconstruction.py || [ $? -eq 4 ]
+  if [ "$?" -ne "0" ] ; then
+    echo "ERROR running juggler"
+    exit 1
+  fi
+fi
+
+if [ -f jana.dot ] ; then cp jana.dot ${JUGGLER_REC_FILE_BASE}.dot ; fi
+
+rootls -t ${JUGGLER_REC_FILE_BASE}.tree.edm4eic.root
