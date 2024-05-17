3369486b09030eb15a60ea6b655c441377aa9ea4
Zachary Sweger <zsweger@berkeley.edu>
2024-05-17 11:17:54 -0700

    Fix simulate.sh

--- benchmarks/u_rho/simulate.sh
+++ benchmarks/u_rho/simulate.sh
@@ -8,13 +8,12 @@ source benchmarks/u_rho/setup.config
 /usr/bin/time -v \
 ddsim --runType run \
       --printLevel WARNING \
-      --enableGun \
-      --steeringFile ${JUGGLER_GEN_FILE} \
       --numberOfEvents ${JUGGLER_N_EVENTS} \
       --part.minimalKineticEnergy 1*TeV  \
       --filter.tracker edep0 \
       --compactFile ${DETECTOR_PATH}/${DETECTOR_CONFIG}.xml \
-      --outputFile  ${JUGGLER_SIM_FILE}
+      --inputFile ${JUGGLER_IN_FILE} \
+      --outputFile  ${JUGGLER_OUT_FILE}
 if [[ "$?" -ne "0" ]] ; then
   echo "ERROR running ddsim"
   exit 1
