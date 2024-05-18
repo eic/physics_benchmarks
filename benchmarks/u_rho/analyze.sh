e5e1db880e429d930951617d62d71b757427427a
Zachary Sweger <zsweger@berkeley.edu>
2024-05-17 17:25:05 -0700

    Create analyze.sh

---
+++ benchmarks/u_rho/analyze.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+source strict-mode.sh
+
+source benchmarks/u_rho/setup.config $*
+
+OUTPUT_PLOTS_DIR=benchmark_output/nocampaign
+mkdir -p ${OUTPUT_PLOTS_DIR}
+# Analyze
+/usr/bin/time -v \
+root -l -b -q benchmarks/u_rho/analysis/uchannelrho.cxx+("${REC_FILE}","${OUTPUT_PLOTS_DIR}/plots.root")
+if [[ "$?" -ne "0" ]] ; then
+  echo "ERROR analysis failed"
+  exit 1
+fi
+
+if [ ! -d "${OUTPUT_PLOTS_DIR}_figures" ]; then
+    mkdir "${OUTPUT_PLOTS_DIR}_figures"
+    echo "${OUTPUT_PLOTS_DIR}_figures directory created successfully."
+else
+    echo "${OUTPUT_PLOTS_DIR}_figures directory already exists."
+fi
+root -l -b -q benchmarks/u_rho/macros/plot_rho_physics_benchmark.C("${OUTPUT_PLOTS_DIR}/plots.root")
+cat benchmark_output/*.json
