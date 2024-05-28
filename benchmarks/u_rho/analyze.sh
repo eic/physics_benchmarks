244cfcb3e998d8b0636736110be8d8f5037622ee
Zachary Sweger <zsweger@berkeley.edu>
2024-05-27 19:31:25 -0700

    Update figures path

--- benchmarks/u_rho/analyze.sh
+++ benchmarks/u_rho/analyze.sh
@@ -14,11 +14,11 @@ if [[ "$?" -ne "0" ]] ; then
   exit 1
 fi
 
-if [ ! -d "${OUTPUT_PLOTS_DIR}_figures" ]; then
-    mkdir "${OUTPUT_PLOTS_DIR}_figures"
-    echo "${OUTPUT_PLOTS_DIR}_figures directory created successfully."
+if [ ! -d "${OUTPUT_PLOTS_DIR}/plots_figures" ]; then
+    mkdir "${OUTPUT_PLOTS_DIR}/plots_figures"
+    echo "${OUTPUT_PLOTS_DIR}/plots_figures directory created successfully."
 else
-    echo "${OUTPUT_PLOTS_DIR}_figures directory already exists."
+    echo "${OUTPUT_PLOTS_DIR}/plots_figures directory already exists."
 fi
 root -l -b -q "benchmarks/u_rho/macros/plot_rho_physics_benchmark.C(\"${OUTPUT_PLOTS_DIR}/plots.root\")"
 cat sim_output/*.json
