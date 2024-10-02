126b023440327cdd2adede0b6bb5a4d63e8a8a4b
Zachary W Sweger <zsweger@eic0108.rcf.bnl.gov>
2024-10-02 15:34:56 -0400

    minbineff and maxbineff to ints

--- benchmarks/your_benchmark/macros/plot_rho_physics_benchmark.C
+++ benchmarks/your_benchmark/macros/plot_rho_physics_benchmark.C
@@ -173,8 +173,8 @@ void plot_rho_physics_benchmark(TString filename="./sim_output/plot_combined.roo
   h_VM_mass_MC_etacut->Draw("HIST E same");
   h_VM_mass_REC_etacut->Draw("HIST E same");
 
-  double minbineff = h_VM_mass_MC_etacut->FindBin(0.6);
-  double maxbineff = h_VM_mass_MC_etacut->FindBin(1.0);
+  int minbineff = h_VM_mass_MC_etacut->FindBin(0.6);
+  int maxbineff = h_VM_mass_MC_etacut->FindBin(1.0);
   double thiseff = 100.0*(1.0*h_VM_mass_REC_etacut->Integral(minbineff,maxbineff))/(1.0*h_VM_mass_MC_etacut->Integral(minbineff,maxbineff));
 
   r42->Draw("same");
@@ -409,8 +409,8 @@ void plot_rho_physics_benchmark(TString filename="./sim_output/plot_combined.roo
   TString figure3name = figure_directory+"/benchmark_rho_efficiencies.pdf";
   c5->Print(figure3name);
 
-  int minbineff = h_VM_mass_MC_etacut->FindBin(0.6);
-  int maxbineff = h_VM_mass_MC_etacut->FindBin(1.0);
+  minbineff = h_VM_mass_MC_etacut->FindBin(0.6);
+  maxbineff = h_VM_mass_MC_etacut->FindBin(1.0);
   double reconstuctionEfficiency = (1.0*h_VM_mass_REC_etacut->Integral(minbineff,maxbineff))/(1.0*h_VM_mass_MC_etacut->Integral(minbineff,maxbineff));
   //set the benchmark status:
   setbenchstatus(reconstuctionEfficiency);
